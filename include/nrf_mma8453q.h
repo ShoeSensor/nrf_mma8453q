a572ff472fbd4e279069dbe2db04d3cdf2c67f73
Bart Monhemius <bart4524@gmail.com>
2016-04-03 15:31:00 +0200

    mma: update implementation
    - Device should be in stanby while configuring
    - Add macro's for all regusters and masks
    - Add the ability to set the datarate, G-force range and resolution

--- include/nrf_mma8453q.h
+++ include/nrf_mma8453q.h
@@ -25,7 +25,61 @@
 extern "C" {
 #endif
 
-#define DRV_ACCEL_CONF_DEFAULT (drv_accelConfig_t) {    \
+#define REG_STATUS              0x00 /**<(R) Real time status*/
+#define REG_OUT_X_MSB           0x01 /**<(R) [7:0] are 8 MSBs of 10-bit sample*/
+#define REG_OUT_X_LSB           0x02 /**<(R) [7:6] are 2 LSBs of 10-bit sample*/
+#define REG_OUT_Y_MSB           0x03 /**<(R) [7:0] are 8 MSBs of 10-bit sample*/
+#define REG_OUT_Y_LSB           0x04 /**<(R) [7:6] are 2 LSBs of 10-bit sample*/
+#define REG_OUT_Z_MSB           0x05 /**<(R) [7:0] are 8 MSBs of 10-bit sample*/
+#define REG_OUT_Z_LSB           0x06 /**<(R) [7:6] are 2 LSBs of 10-bit sample*/
+#define REG_SYSMOD              0x0b /**<(R) Current system mode*/
+#define REG_INT_SOURCE          0x0c /**<(R) Interrupt status*/
+#define REG_WHO_AM_I            0x0d /**<(R) Device ID (0x3A)*/
+#define REG_XYZ_DATA_CFG        0x0e /**<(R/W) Dynamic range settings*/
+#define REG_HP_FILTER_CUTOFF    0x0f /**<(R/W) cut-off frequency is set to 16Hz @ 800Hz*/
+#define REG_PL_STATUS           0x10 /**<(R) Landscape/Portrait orientation status*/
+#define REG_PL_CFG              0x11 /**<(R/W) Landscape/Portrait configuration*/
+#define REG_PL_COUNT            0x12 /**<(R) Landscape/Portrait debounce counter*/
+#define REG_PL_BF_ZCOMP         0x13 /**<(R) Back-Front, Z-Lock trip threshold*/
+#define REG_P_L_THS_REG         0x14 /**<(R/W) Portrait to Landscape trip angle is 29 degree*/
+#define REG_FF_MT_CFG           0x15 /**<(R/W) Freefall/motion functional block configuration*/
+#define REG_FF_MT_SRC           0x16 /**<(R) Freefall/motion event source register*/
+#define REG_FF_MT_THS           0x17 /**<(R/W) Freefall/motion threshold register*/
+#define REG_FF_MT_COUNT         0x18 /**<(R/W) Freefall/motion debounce counter*/
+#define REG_TRANSIENT_CFG       0x1d /**<(R/W) Transient functional block configuration*/
+#define REG_TRANSIENT_SRC       0x1e /**<(R) Transient event status register*/
+#define REG_TRANSIENT_THS       0x1f /**<(R/W) Transient event threshold*/
+#define REG_TRANSIENT_COUNT     0x20 /**<(R/W) Transient debounce counter*/
+#define REG_PULSE_CFG           0x21 /**<(R/W) ELE, Double_XYZ or Single_XYZ*/
+#define REG_PULSE_SRC           0x22 /**<(R) EA, Double_XYZ or Single_XYZ*/
+#define REG_PULSE_THSX          0x23 /**<(R/W) X pulse threshold*/
+#define REG_PULSE_THSY          0x24 /**<(R/W) Y pulse threshold*/
+#define REG_PULSE_THSZ          0x25 /**<(R/W) Z pulse threshold*/
+#define REG_PULSE_TMLT          0x26 /**<(R/W) Time limit for pulse*/
+#define REG_PULSE_LTCY          0x27 /**<(R/W) Latency time for 2nd pulse*/
+#define REG_PULSE_WIND          0x28 /**<(R/W) Window time for 2nd pulse*/
+#define REG_ASLP_COUNT          0x29 /**<(R/W) Counter setting for auto-sleep*/
+#define REG_CTRL_REG1           0x2a /**<(R/W) ODR = 800 Hz, STANDBY mode*/
+#define REG_CTRL_REG2           0x2b /**<(R/W) Sleep enable, OS Modes, RST, ST*/
+#define REG_CTRL_REG3           0x2c /**<(R/W) Wake from sleep, IPOL, PP_OD*/
+#define REG_CTRL_REG4           0x2d /**<(R/W) Interrupt enable register*/
+#define REG_CTRL_REG5           0x2e /**<(R/W) Interrupt pin (INT1/INT2) map*/
+#define REG_OFF_X               0x2f /**<(R/W) X-axis offset adjust*/
+#define REG_OFF_Y               0x30 /**<(R/W) Y-axis offset adjust*/
+#define REG_OFF_Z               0x31 /**<(R/W) Z-axis offset adjust*/
+#define FULL_SCALE_RANGE_2g     0x00
+#define FULL_SCALE_RANGE_4g     0x01
+#define FULL_SCALE_RANGE_8g     0x02
+#define ACTIVE_MASK             1
+#define RES_MASK                2
+#define DATA_RATE_MASK          0xC7
+#define DATA_RATE_800           0
+#define DATA_RATE_400           1
+#define DATA_RATE_200           2
+#define DATA_RATE_100           3
+#define DATA_RATE_50            4
+
+#define DRV_TWI_CONF_DEFAULT (drv_accelConfig_t) {    \
         .sdaPin = TWI0_CONFIG_SDA,                      \
         .sclPin = TWI0_CONFIG_SCL,                      \
         .twiFreq = TWI0_CONFIG_FREQUENCY,               \
@@ -40,6 +94,12 @@ typedef struct {
     nrf_twi_frequency_t twiFreq;
     uint8_t id;
     bool enable;
+} drv_twiConfig_t;
+
+typedef struct {
+    uint8_t gRange;
+    bool highRes;
+    uint8_t samplingRate;
 } drv_accelConfig_t;
 
 typedef struct {
@@ -56,7 +116,7 @@ typedef struct {
  * @param conf [description]
  * @return [description]
  */
-drv_accelHandle_t drv_accelInit(drv_accelConfig_t *conf);
+drv_accelHandle_t drv_accelInit(drv_twiConfig_t *conf);
 
 /**
  *
@@ -74,10 +134,8 @@ void drv_accelEnable(drv_accelHandle_t handle);
  * @param value [description]
  * @return [description]
  */
-bool drv_accelSetup(drv_accelHandle_t handle,
-        uint8_t adress,
-        uint8_t data,
-        uint8_t value);
+bool drv_accelSetup(drv_accelHandle_t handle, uint8_t adress,
+    drv_accelConfig_t *conf);
 
 /**
  * @brief [brief description]
--- src/nrf_mma8453q.c
+++ src/nrf_mma8453q.c
@@ -33,11 +33,35 @@
 struct drv_accelHandle {
     nrf_drv_twi_t instance;
     uint8_t address;
+    bool highRes;
 };
 
 const static uint8_t accelRegVal = 1;
 
-drv_accelHandle_t drv_accelInit(drv_accelConfig_t *conf)
+
+static void setStandby(drv_accelHandle_t handle)
+{
+    uint8_t response[2] = {REG_CTRL_REG1, 0};;
+    nrf_drv_twi_rx(&handle->instance, handle->address, &response[1], 1, true);
+    response[1] &= ~ACTIVE_MASK;
+    nrf_drv_twi_tx(&handle->instance, handle->address, response, 2, true);
+}
+
+static void setActive(drv_accelHandle_t handle)
+{
+    uint8_t response[2] = {REG_CTRL_REG1, 0};
+    nrf_drv_twi_rx(&handle->instance, handle->address, &response[1], 1, true);
+    response[1] |= ACTIVE_MASK;
+    nrf_drv_twi_tx(&handle->instance, handle->address, response, 2, true);
+}
+
+uint32_t setReg(drv_accelHandle_t handle, uint8_t data[2])
+{
+    return nrf_drv_twi_tx(&handle->instance, handle->address,
+        data, 2, true);
+}
+
+drv_accelHandle_t drv_accelInit(drv_twiConfig_t *conf)
 {
     drv_accelHandle_t handle = calloc(1, sizeof(nrf_drv_twi_t));
     nrf_drv_twi_config_t twiConf = {
@@ -58,46 +82,85 @@ void drv_accelEnable(drv_accelHandle_t handle)
     nrf_drv_twi_enable(&handle->instance);
 }
 
-bool drv_accelSetup(drv_accelHandle_t handle, uint8_t address, uint8_t data,
-        uint8_t value)
+bool drv_accelConfigure(drv_accelHandle_t handle, uint8_t address,
+        drv_accelConfig_t *conf)
 {
+    uint8_t response;
     uint32_t errCode;
-    uint8_t accelBuf[] = { data, value };
     handle->address = address;
-    errCode = nrf_drv_twi_tx(&handle->instance,
-        address,
-        accelBuf,
-        2,
-        false);
-    return (errCode == NRF_SUCCESS);
+    setStandby(handle);
+    // Range
+    errCode = setReg(handle, (uint8_t[]){REG_XYZ_DATA_CFG, conf->gRange});
+    if(errCode != NRF_SUCCESS)
+        return false;
+    // Resolution
+    errCode = nrf_drv_twi_tx(&handle->instance, handle->address,
+            (uint8_t[]){REG_CTRL_REG1}, 1, false);
+    if(errCode != NRF_SUCCESS)
+        return false;
+    if(conf->highRes) {
+        errCode = nrf_drv_twi_rx(&handle->instance,
+                handle->address, &response, 1, true);
+        setReg(handle, (uint8_t[]){REG_CTRL_REG1, response &~RES_MASK});
+    } else {
+        errCode = nrf_drv_twi_rx(&handle->instance,
+                handle->address, &response, 1, true);
+        setReg(handle, (uint8_t[]){REG_CTRL_REG1, response | RES_MASK});
+    }
+    if(errCode != NRF_SUCCESS)
+        return false;
+    errCode = nrf_drv_twi_rx(&handle->instance,
+            handle->address, &response, 1, true);\
+    if(errCode != NRF_SUCCESS)
+        return false;
+    errCode = setReg(handle, (uint8_t[]){REG_CTRL_REG1,
+            (response & DATA_RATE_MASK) | (conf->samplingRate << 3)});
+    if(errCode != NRF_SUCCESS)
+        return false;
+    setActive(handle);
+    return (NRF_SUCCESS);
 }
 
 drv_accelData_t drv_accelRead(drv_accelHandle_t handle)
 {
     drv_accelData_t accelData;
     uint32_t errCode;
-    uint8_t accelBuf[7];
+    uint8_t accelBuf[6];
 
     memset(accelBuf, 0, sizeof(accelBuf));
-    memset(&accelData, 0, sizeof(accelData));
-    accelBuf[0] = accelRegVal;
 
-    errCode = nrf_drv_twi_rx(&handle->instance,
-            handle->address,
-            accelBuf,
-            7,
-            false);
+    errCode = nrf_drv_twi_tx(&handle->instance, handle->address,
+            (uint8_t[]){REG_CTRL_REG1}, 1, false);
 
-    if (errCode != NRF_SUCCESS) {
-        accelData.failed = true;
-        return accelData;
+    if (errCode != NRF_SUCCESS)
+        goto err;
+
+    if(handle->highRes) {
+        errCode = nrf_drv_twi_rx(&handle->instance, handle->address,
+                accelBuf, 6, true);
+        accelData.x = (accelBuf[0] << 4 | (accelBuf[1] >> 4 & 0xE));
+        accelData.y = (accelBuf[2] << 4 | (accelBuf[3] >> 4 & 0xE));
+        accelData.z = (accelBuf[4] << 4 | (accelBuf[5] >> 4 & 0xE));
+    } else {
+        errCode = nrf_drv_twi_rx(&handle->instance, handle->address,
+                accelBuf, 3, true);
+        accelData.x = (accelBuf[0] << 4);
+        accelData.y = (accelBuf[1] << 4);
+        accelData.z = (accelBuf[2] << 4);
     }
 
-    accelData.x = (accelBuf[0] << 4 | (accelBuf[1] >> 4 & 0xE));
-    accelData.y = (accelBuf[2] << 4 | (accelBuf[3] >> 4 & 0xE));
-    accelData.z = (accelBuf[4] << 4 | (accelBuf[5] >> 4 & 0xE));
+    if (errCode != NRF_SUCCESS)
+        goto err;
+
+    accelData.x = (accelData.x > 2047) ? accelData.x - 4096 : accelData.x;
+    accelData.y = (accelData.y > 2047) ? accelData.y - 4096 : accelData.y;
+    accelData.z = (accelData.z > 2047) ? accelData.z - 4096 : accelData.z;
 
     return accelData;
+
+    err:
+        accelData.failed = true;
+        return accelData;
 }
 
 void drv_accelDisable(drv_accelHandle_t handle)
